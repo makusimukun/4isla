0d8e30ae8a43a09b41391e856580e3afdfff9e9e
unknown <maksimatveev92@mail.ru>
2023-03-22 13:37:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = new int[5];
+int min = int.MaxValue;
+int max = int.MinValue;
+double avg = 0.0;
+int sum = 0;
+int mult = 1;
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Введите число: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < 5; i++)
+{
+    if (arr[i] < min)
+    {
+        min = arr[i];
+    }
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    sum += arr[i];
+    mult *= arr[i];
+}
+avg = (double)sum / arr.Length;
+Console.WriteLine("Минимальное значение: " + min);
+Console.WriteLine("Максимальное значение: " + max);
+Console.WriteLine("Среднее значение: " + avg);
+Console.WriteLine("Сумма: " + sum);
+Console.WriteLine("Произведение: " + mult);
